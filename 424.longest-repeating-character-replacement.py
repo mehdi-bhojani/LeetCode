56947d36f2f041ecf807eae0d72bd5980f64a53b
Muhammad Mehdi Bhojani <108871383+mehdi-bhojani@users.noreply.github.com>
2026-07-12 03:07:44 +0500

    Update 424.longest-repeating-character-replacement.py

--- 424.longest-repeating-character-replacement.py
+++ 424.longest-repeating-character-replacement.py
@@ -7,15 +7,29 @@
 # @lc code=start
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = right = 0
-        occurances = [0]*26
-        ans = 0
-        max_occurance = 0
+        left = right = 1
+        myDictionary = {}
+        result = 0
+        length = len(s)
+        ans=0
+        max_occurance=0
 
-        for i in range(len(s)):
-            occurances[s[i]-'A'] += +1
-            max_occurance = max(max_occurance, occurances[s[i]-'A'])
-            ans = 
+        while(left<=length):
+            if s[left-1] not in myDictionary:
+                myDictionary[s[left-1]]=0
+            myDictionary[s[left-1]] += 1
+            max_occurance = max(max_occurance,myDictionary[s[left-1]])
+            print(max_occurance)
+            if (left - right + 1 - max_occurance) <= k:
+                ans = left - right + 1
+            else: 
+                myDictionary[s[right-1]]=myDictionary[s[right-1]]-1
+                right=right+1
+            result= max(result,ans)
+            left=left+1
+        
+        return result
         
 # @lc code=end
+
 
